62c103dc72407108e2f0767d1244d17940281668
aaronsw@frabjous-2.local <aaronsw@frabjous-2.local>
2006-11-07 12:12:50 -0500

    cleanup links code

--- core/view.py
+++ core/view.py
@@ -1,29 +1,10 @@
 import re
-import markdown
+from utils import markdown
 import web
 from utils import delegate
 
 def keyencode(text): return text.replace(' ', '_')
 def keydecode(text): return text.replace('_', ' ')
-
-link_re = web.re_compile(r'(?<!\\)\[\[(.*?)(?:\|(.*?))?\]\]')
-def do_links(text, links=False):
-    linksto = []
-
-    #@@ needs to not replace in <pre> and so on
-
-    def mangle(match):
-        t = match.group()
-        matches = match.groups()
-        link = matches[0]
-        anchor = matches[1] or link
-        link = keyencode(link)
-        linksto.append(link)
-        return '<a class="internal" href="%s/%s">%s</a>' % (web.ctx.homepath, link, anchor)
-
-    text = re.sub(link_re, mangle, text)
-    if links: return linksto
-    return text
 
 def get_markdown(text):
    md = markdown.Markdown(source=text, safe_mode=False)
---
+++ plugins/links/code.py
@@ -0,0 +1,26 @@
+"""
+links: allow interwiki links
+
+Adds a markdown preprocessor to catch `[[foo]]` style links.
+Creates a new set of database tables to keep track of them.
+Creates a new `m=backlinks` to display the results.
+"""
+
+from utils import delegate
+import db
+import web
+import view
+import re
+
+render = web.template.render('plugins/links/templates/')
+
+class hooks:
+    __metaclass__ = delegate.hook
+    def on_new_version(site, path, data):
+        db.new_links(site, path, view.get_links(data))
+
+class backlinks (delegate.mode):
+    def GET(self, site, path):
+        links = db.get_links(site, path)
+        print render.backlinks(links)
+

6cbb0ba5c7973547be0af9089511d9a80ef46f6e
anand@anand.futurelabs <anand@anand.futurelabs>
2006-11-16 09:20:40 -0800

    oops. forgot to add auth.py

---
+++ core/auth.py
@@ -0,0 +1,23 @@
+import db
+import time, datetime
+import hmac
+import web
+
+def setcookie(user):
+    t = datetime.datetime(*time.gmtime()[:6]).isoformat()
+    text = "%d,%s" % (user.id, t)
+    text += "," + _digest(text)
+    web.setcookie("infogami_session", text)
+    print >> web.debug, 'setcookie', text
+    
+def get_user():
+    """Returns the current user from the session."""
+    session = web.cookies(infogami_session=None).infogami_session
+    if session:
+        user_id, login_time, digest = session.split(',')
+        if _digest(user_id + "," + login_time) == digest:
+            return db.get_user(int(user_id))
+
+def _digest(text):
+    print >> web.debug, 'digest', text
+    return hmac.HMAC(text).hexdigest()

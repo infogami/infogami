41a6cf2d74df76c721f881d904123cc3e8522e73
aaronsw@frabjous.local <aaronsw@frabjous.local>
2007-01-31 23:57:11 -0800

    review table

--- db_upgrade.py
+++ db_upgrade.py
@@ -104,6 +104,19 @@ def add_review_table():
             unique (site_id, page_id, user_id)
         )""")
 
+@upgrade
+def add_review_table():
+    """adding review column to support user review for pages."""
+    web.query("""
+        CREATE TABLE review (
+            id serial primary key,
+            site_id int references site,
+            page_id int references page,
+            user_id int references login,
+            revision int,
+            unique (site_id, page_id, user_id)
+        )""")
+
 if __name__ == "__main__":
     web.load()
     apply_upgrades()

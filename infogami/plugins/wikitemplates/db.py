7b34856952d9173da02179358b40f35e7ac34ded
anand@sudhama.local <anand@sudhama.local>
2007-06-26 11:57:20 +0530

    fixed macro loading bug.

--- infogami/plugins/wikitemplates/db.py
+++ infogami/plugins/wikitemplates/db.py
@@ -8,4 +8,4 @@ def get_all_macros(site):
     return tdb.Things(type=db.get_type(site, 'type/macro'), parent=site)
     
 def get_all_sites():
-    return tdb.Things(type=tdb.root, parent=tdb.root)
+    return [t for t in tdb.Things(type=tdb.root, parent=tdb.root) if t.id != 1]

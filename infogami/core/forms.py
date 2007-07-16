466b4db89f86d621b226d7bd6481e9faf2915e7d
anand@ia301537.us.archive.org <anand@ia301537.us.archive.org>
2007-07-16 18:06:53 +0000

    constrain characters available in username

--- infogami/core/forms.py
+++ infogami/core/forms.py
@@ -12,15 +12,19 @@ login = Form(
     Checkbox('remember', description=_.REMEMBER_ME)
 )
 
+vlogin = regexp(r"^[A-Za-z0-9-_]{3,20}$", 'must be between 3 and 20 letters and numbers') 
+vpass = regexp(r".{3,20}", 'must be between 3 and 20 characters')
+
 register = Form(
     Textbox('username', 
             Validator(
                 _.USERNAME_ALREADY_EXISTS,
                 lambda name: not db.get_user_by_name(context.site, name)),
+            vlogin,
             description=_.USERNAME),
     Textbox('displayname', notnull, description=_.DISPLAYNAME),
     Textbox('email', notnull, description=_.EMAIL),
-    Password('password', notnull, description=_.PASSWORD),
+    Password('password', notnull, vpass, description=_.PASSWORD),
     Password('password2', notnull, description=_.CONFIRM_PASSWORD),
     validators = [
         Validator(_.PASSWORDS_DID_NOT_MATCH, lambda i: i.password == i.password2)]    

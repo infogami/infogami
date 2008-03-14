e8b44740f468c5cf582ea90f48e74f5dcc4b73c0
anand@vihari.local <anand@vihari.local>
2008-03-14 15:09:48 -0700

    sample_run.py for infogami.new

--- sample_run.py
+++ sample_run.py
@@ -8,6 +8,7 @@ infogami.config.db_parameters = dict(dbn='postgres', db="infogami", user='yourna
 
 ## site name 
 infogami.config.site = 'infogami.org'
+infogami.config.admin_password = "admin123"
 
 ## add additional plugins and plugin path
 #infogami.config.plugin_path += ['plugins']
@@ -16,6 +17,13 @@ infogami.config.site = 'infogami.org'
 ## incase you want to generate tdb log
 #import infogami.tdb.tdb
 #infogami.tdb.logger.set_logfile(open("tdb.log", "a"))
+
+@infogami.install_hook
+@infogami.action
+def createsite():
+    from infogami.infobase import Infobase
+    web.load()
+    Infobase().create_site(infobase.config.site, infobase.config.admin_password)
 
 if __name__ == "__main__":
     infogami.run()

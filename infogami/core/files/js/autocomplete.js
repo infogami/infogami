1f1a20b9549e234da0fe77013c3eee2b3f50cc6b
anand@bodhi.local <anand@bodhi.local>
2008-04-10 11:15:50 +0530

    changed autocomplete to infogami_autocomplete to avoid name conflicts

--- infogami/core/files/js/autocomplete.js
+++ infogami/core/files/js/autocomplete.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-    jQuery('input[@autocomplete=true]').each(function(){
+    jQuery('input[@infogami_autocomplete=true]').each(function(){
 
         var thingtype = jQuery(this).attr('thingtype')
         var maxlimit  = jQuery(this).attr('limit')

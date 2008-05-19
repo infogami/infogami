9a5da1a5425f76f526b43b3824a1211885a61b5f
anand@bodhi <anand@bodhi>
2008-05-19 15:02:37 +0530

    real fix to JSON unicode/utf-8 issue.

--- infogami/infobase/_json.py
+++ infogami/infobase/_json.py
@@ -8,11 +8,24 @@ See Bug#231831 for details.
     u'\u1234'
     >>> loads(dumps(u'\u1234'.encode('utf-8')))
     u'\u1234'
+    >>> loads(dumps({'x': u'\u1234'.encode('utf-8')}))
+    {u'x': u'\u1234'}
 """
 import simplejson
 
+def unicodify(d):
+    """Converts all utf-8 encoded strings to unicode recursively."""
+    if isinstance(d, dict):
+        return dict((k, unicodify(v)) for k, v in d.iteritems())
+    elif isinstance(d, list):
+        return [unicodify(x) for x in d]
+    elif isinstance(d, str):
+        return d.decode('utf-8')
+    else:
+        return d
+
 def dumps(obj, **kw):
-    return simplejson.dumps(obj, ensure_ascii=False, *kw)
+    return simplejson.dumps(unicodify(obj), **kw)
 
 def loads(s, **kw):
     return simplejson.loads(s, **kw)

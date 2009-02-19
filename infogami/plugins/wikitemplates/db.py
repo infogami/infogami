ba1e3ab4f06fb486e5032cfe05e5ce37a0c253e5
Anand Chitipothu <anandology@gmail.com>
2009-02-19 18:31:06 +0530

    Bad typo.

--- infogami/plugins/wikitemplates/db.py
+++ infogami/plugins/wikitemplates/db.py
@@ -12,7 +12,7 @@ def get_all_macros(site):
     t = site.get('/type/macro')
     if t is None:
         return []
-    q = {'type': '/type/macro', 'limig': 1000}
+    q = {'type': '/type/macro', 'limit': 1000}
     #return [site.get(key) for key in site.things(q)]
     return site.get_many([key for key in site.things(q)])
     

6d7a29454701fa96590c19521831e771119b6559
Anand Chitipothu <anandology@gmail.com>
2009-02-20 15:33:13 +0530

    fix and improve migration script

--- migration/migrate-0.4-0.5.py
+++ migration/migrate-0.4-0.5.py
@@ -32,11 +32,19 @@ create table property (
 );
 """
 
+#@@ type to table prefix mappings. 
+#@@ If there are any special tables in your schema, this should be updated.
+type2table = {
+
+}
+
 def get_table_prefix(type):
     """Returns table prefix for that type and a boolean flag to specify whether the table has more than one type.
     When the table as values only from a single type, then some of the queries can be optimized.
     """
-    return "datum", True
+    table = type2table.get(type, 'datum')
+    multiple = (table == 'datum') or type2table.values().count(table) > 1    
+    return table, multiple
 
 def fix_property_keys():
     """In 0.4, property keys are stored in $prefix_keys table where as in 0.5
@@ -57,14 +65,14 @@ def fix_property_keys():
         
         for d in ['str', 'int', 'float', 'boolean', 'ref']:
             table = prefix + '_' + d            
-            print >> web.debug, 'fixing table', table
+            print >> web.debug, 'fixing', type, table
             for key in keys:
                 old_key_id = keys[key]
                 new_key_id = newkeys[key]
                 if multiple_types:
                     db.query('UPDATE %s SET key_id=$new_key_id FROM thing WHERE thing.id = %s.thing_id AND thing.type=$type_id AND key_id=$old_key_id' % (table, table), vars=locals())
                 else:
-                    web.update(table, key_id=new_key_id, where='key_id=$old_key_id', vars=locals())    
+                    db.update(table, key_id=new_key_id, where='key_id=$old_key_id', vars=locals())    
                 
     primitive = ['/type/key', '/type/int', '/type/float', '/type/boolean', '/type/string', '/type/datetime']
     # add embeddable types too
@@ -75,7 +83,7 @@ def fix_property_keys():
         fix_type(type, types[type])
         
 def drop_key_id_foreign_key():
-    table_prefixes = ['datum']
+    table_prefixes = set(type2table.values() + ['datum'])
     for prefix in table_prefixes:
         for d in DATATYPES:
             table = prefix + '_' + d

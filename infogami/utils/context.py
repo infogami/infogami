4616b46333bb1b9068c64be06e05306bd9ed1900
Open Library Team <openlibrary@anandology.com>
2009-09-22 20:03:01 +0530

    make context a ThreadedDict

--- infogami/utils/context.py
+++ infogami/utils/context.py
@@ -3,19 +3,23 @@ Threaded context for infogami.
 """
 import web
 
-class InfogamiContext:
+class InfogamiContext(web.ThreadedDict):
     """
     Threaded context for infogami.
     Uses web.ctx for providing a thread-specific context for infogami.
     """
+    def load(self):
+        pass
+    '''
     def __getattr__(self, key):
         return getattr(web.ctx.infogami_ctx, key)
 
     def __setattr__(self, key, value):
         setattr(web.ctx.infogami_ctx, key, value)
-
+    
     def load(self):
         """Initializes context for the calling thread."""
         web.ctx.infogami_ctx = web.storage()
-
+    '''
+    
 context = InfogamiContext()

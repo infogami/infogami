decfa15b3435992666adc5783b092288efcc5904
Anand Chitipothu <anandology@gmail.com>
2010-09-01 11:27:39 -0700

    measure total time and unaccounted time

--- infogami/utils/stats.py
+++ infogami/utils/stats.py
@@ -5,6 +5,8 @@ Here is an example usage:
     stats.begin("memcache", method="get", key="foo")
     memcache_client.get("foo")
     stats.end()
+
+Currently this doesn't support nesting.    
 """
 import web
 import time
@@ -29,11 +31,21 @@ def end(**kw):
     
 def stats_summary():
     d = web.storage()
+
+    if not web.ctx.get("stats"):
+        return d
+        
+    total_measured = 0.0
     
-    for s in web.ctx.get("stats", []):
+    for s in web.ctx.stats:
         if s.name not in d:
             d[s.name] = web.storage(count=0, time=0.0)
         d[s.name].count += 1
         d[s.name].time += s.time
-        
+        total_measured += s.time
+    
+    # consider the start time of first stat as start of the request
+    total_time = time.time() - web.ctx.stats[0].t_start
+    d['total'] = web.storage(count=0, time=total_time, unaccounted=total_time-total_measured)
+    
     return d

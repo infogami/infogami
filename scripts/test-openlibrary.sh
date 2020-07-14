38d2fcda34504327bbb1e01fa7460fd553631a7e
Drini Cami <cdrini@gmail.com>
2020-07-14 12:59:19 -0400

    Test openlibrary in travis for regressions as well

---
+++ scripts/test-openlibrary.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run from the infogami root dir
+cd ..
+OL_DIR=openlibrary-$RANDOM
+git clone https://github.com/internetarchive/openlibrary $OL_DIR
+rm -rf $OL_DIR/vendor/infogami
+ln -sf $(pwd)/infogami $OL_DIR/vendor/infogami
+
+cd $OL_DIR
+pip install -r requirements_test.txt
+make test

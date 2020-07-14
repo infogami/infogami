26f19e4752efca5eb96221397a8d3107a29675e5
Drini Cami <cdrini@gmail.com>
2020-07-14 18:38:55 -0400

    Document + fix bugs in test-openlibrary.sh
    - The symlinking needs to likely happen after make git.
    - Use bash -x to display commands as they are being run

--- scripts/test-openlibrary.sh
+++ scripts/test-openlibrary.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Run from the infogami root dir
+# Clone latest openlibrary adjacent to where infogami is
 cd ..
+# Add a random number to the end here, so that it can run ~in parallel (?)
 OL_DIR=openlibrary-$RANDOM
 git clone https://github.com/internetarchive/openlibrary $OL_DIR
+
+# Make it use the current infogami
+pushd $OL_DIR && make git && popd;
 rm -rf $OL_DIR/vendor/infogami
 ln -sf $(pwd)/infogami $OL_DIR/vendor/infogami
 
+# Install requirements / run pytests
 cd $OL_DIR
-make git
 pip install -r requirements_test.txt
+# TODO: Use make test-py once https://github.com/internetarchive/openlibrary/pull/3582 merged
 pytest openlibrary/tests openlibrary/mocks openlibrary/olbase openlibrary/plugins openlibrary/utils openlibrary/catalog openlibrary/coverstore scripts/tests
